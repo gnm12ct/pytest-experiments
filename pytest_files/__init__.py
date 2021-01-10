a12ab3d8dc51ecde175c5eac9c46b5b02f11d3cf
Nogueras Gonzalo <gnm12ct@gmail.com>
2021-01-10 17:51:31 +0000

    more initial files

--- pytest_files/__init__.py
+++ pytest_files/__init__.py
@@ -1,7 +1,7 @@
 """
 Configure assertion rewrites
 
-Pytest needs to be told which modules need asserions rewruitting. This
+Pytest needs to be told which modules need assertions rewritting. This
 enables detailed traceback information to be returned, such as showing
 the differences in expected and actual results. Any modules with
 assertions used in the plugin need to be specified here.
